6f082d90c0593a86c0dbccd4fee18e406050f307
unknown <behrouzmaasser1@gmail.com>
2022-04-02 14:00:30 +0300

    Initial commit.

---
+++ wReminder_tkinter_mvc/wReminder/core/controller.py
@@ -0,0 +1,92 @@
+import os
+import datetime as dt
+import pytz
+import platform as plt
+from plyer import notification
+
+from threading import Timer
+
+from wReminder_tkinter_mvc.core.reminder_model import (
+    Reminder, validate_inputs
+)
+
+
+# Controller
+class Controller:
+    def __init__(self):
+        self.view = None
+        self.reminders = []
+        self.reminder_id_counter = 0
+
+    def add_reminder(
+            self, timezone_listbox, calendar, hours_entry, minutes_entry
+    ):
+        # Get mo
+        result = validate_inputs(
+            timezone_listbox, calendar, hours_entry, minutes_entry
+        )
+        if result['data']:
+            self.reminder_id_counter += 1
+            reminder = Reminder(
+                result['data'][0], result['data'][1], self.reminder_id_counter
+            )
+            self.reminders.append(reminder)
+
+            # Sort the reminders based on reminder time zone
+            self.reminders.sort(
+                key=lambda rem: str(rem.datetime_in_country.tzinfo)
+            )
+            self.run_reminder(reminder)
+            self.view.display_message(
+                message_type='success', message="Reminder's Set Successfully!"
+            )
+        else:
+            self.view.display_message(
+                message_type='error', message=result['error']
+            )
+
+    def run_reminder(self, reminder):
+        time_difference = \
+            reminder.datetime_in_utc - dt.datetime.now(tz=pytz.utc)
+        reminder.timer_obj = Timer(
+            time_difference.seconds, self.run_notify, args=[
+                self.view.frames['0'].reminder_notification_title.get(),
+                self.view.frames['0'].reminder_notification_message.get(),
+                reminder.reminder_id
+            ])
+        reminder.timer_obj.start()
+
+    def run_notify(self, title, message, reminder_id):
+        notify(title, message)
+        self.cancel_reminder(reminder_id)
+
+    def cancel_reminder(self, reminder_id):
+        for index, reminder in enumerate(self.reminders):
+            if reminder.reminder_id == reminder_id:
+                reminder.timer_obj.cancel()
+                self.reminders.pop(index)
+
+                # Sort the reminders based on reminder time zone
+                self.reminders.sort(
+                    key=lambda rem: str(rem.datetime_in_country.tzinfo)
+                )
+                self.view.change_frame(self.view.current_frame_on)
+                break
+
+
+def notify(title, message):
+    if plt.system() == 'Windows':
+        notification.notify(
+            title=title,
+            message=message,
+            app_icon='app_icon.ico',
+            timeout=6
+        )
+    elif plt.system() == 'Darwin':
+        os.system(f'''
+                        osascript -e 'display notification "{message}" with title "{title}" sound name "Submarine"'
+                ''')
+    elif plt.system() == 'Linux':
+        os.system(f'''
+                        notify-send "{message}" "{title}"'
+                ''')
---
+++ wReminder_tkinter_mvc/wReminder/core/view.py
@@ -0,0 +1,231 @@
+import datetime as dt
+import pytz
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo, showerror
+from tkcalendar import Calendar
+
+
+# Control Frame
+class ControlFrame(ttk.LabelFrame):
+    def __init__(self, container, controller):
+
+        super().__init__(container)
+        self['text'] = 'Options'
+        self.container = container
+        self.controller = controller
+        self.current_frame_on = '1'
+
+        # initialize frames
+        self.frames = {
+            '0': AddReminder(container, self.controller),
+            '1': DisplayReminders(container, self.controller)
+        }
+
+        # Display Reminders Button
+        display_reminders_menu_button = ttk.Button(
+            self,
+            text="Display Reminders",
+            command=lambda: self.change_frame('1')
+        )
+        display_reminders_menu_button.grid(column=0, row=0, padx=5, pady=5)
+
+        # Add Reminder Button
+        add_reminder_menu_button = ttk.Button(
+            self,
+            text="Add Reminder",
+            command=lambda: self.change_frame('0')
+        )
+        add_reminder_menu_button.grid(column=1, row=0, padx=5, pady=5)
+
+        # Set The Control Frame
+        self.grid(column=0, row=0, padx=5, pady=5, sticky='w')
+
+    # Switch to Frames Add Reminder or Display Reminders
+    def change_frame(self, frame_number):
+        if frame_number == '1':
+            self.frames['1'] = DisplayReminders(
+                self.container, self.controller
+            )
+        self.frames[frame_number].tkraise()
+        self.current_frame_on = frame_number
+
+    # Display Message Dialogs to User
+    def display_message(self, message_type, message, title=''):
+        if message_type == 'error':
+            showerror(title, message)
+        elif message_type == 'success':
+            showinfo(title, message)
+
+
+# Add Reminder Frame
+class AddReminder(ttk.Frame):
+    def __init__(self, container, controller):
+        super().__init__(container)
+
+        # Options
+        options = {'padx': 5, 'pady': 5}
+        time_font = ('Times', 16)
+
+        # Reminder notification title Label
+        self.reminder_notification_title_label = ttk.Label(
+            self, text='Reminder Title: '
+        )
+        self.reminder_notification_title_label.grid(
+            column=0, row=1, sticky='w', **options
+        )
+
+        # Reminder notification title Entry
+        self.reminder_notification_title = tk.StringVar()
+        self.reminder_notification_title_entry = ttk.Entry(
+            self,
+            textvariable=self.reminder_notification_title
+        )
+        self.reminder_notification_title_entry.grid(
+            column=1, columnspan=1, row=1, sticky='w', **options
+        )
+        self.reminder_notification_title_entry.focus()
+
+        # Reminder notification message Label
+        self.reminder_notification_message_label = ttk.Label(
+            self, text='Reminder Message: '
+        )
+        self.reminder_notification_message_label.grid(
+            column=2, row=1, sticky='w', **options
+        )
+
+        # Reminder notification message Entry
+        self.reminder_notification_message = tk.StringVar()
+        self.reminder_notification_message_entry = ttk.Entry(
+            self,
+            textvariable=self.reminder_notification_message
+        )
+        self.reminder_notification_message_entry.grid(
+            column=3, columnspan=1, row=1, sticky='w', **options
+        )
+
+        # Date Label
+        self.country_year_label = ttk.Label(self, text='Date: ')
+        self.country_year_label.grid(column=0, row=2, sticky='w', **options)
+
+        # Calendar
+        self.cal = Calendar(
+            self,
+            selectmode='day',
+            year=dt.datetime.now().year,
+            month=dt.datetime.now().month,
+            day=dt.datetime.now().day
+        )
+        self.cal.grid(
+            column=1, row=2, columnspan=3, rowspan=4, sticky='nsew', **options
+        )
+
+        # Hour Label
+        self.hour_label = ttk.Label(self, text='Hour : ')
+        self.hour_label.grid(column=0, row=7, sticky='w', **options)
+
+        # Hour Entry
+        self.hours = tk.StringVar(value=tuple(range(24)))
+        self.hours_entry = tk.Listbox(
+            self,
+            listvariable=self.hours,
+            font=time_font, justify='center', exportselection=0, height=4
+        )
+        self.hours_entry.grid(column=1, row=7, sticky='w', **options)
+
+        # Minute Label
+        self.hour_label = ttk.Label(self, text='Minute : ')
+        self.hour_label.grid(column=2, row=7, sticky='w', **options)
+
+        # Minute Entry
+        self.minutes = tk.StringVar(value=tuple(range(60)))
+        self.minutes_entry = tk.Listbox(
+            self,
+            listvariable=self.minutes,
+            font=time_font, justify='center', exportselection=0, height=4
+        )
+        self.minutes_entry.grid(column=3, row=7, sticky='w', **options)
+
+        # Timezone Label
+        self.timezone_label = ttk.Label(self, text='Select a Timezone: ')
+        self.timezone_label.grid(column=0, row=8, sticky='w', **options)
+
+        # Timezone ListBox
+        self.timezones = tk.StringVar(value=tuple(pytz.all_timezones))
+        self.timezone_listbox = tk.Listbox(
+            self,
+            height=10,
+            listvariable=self.timezones,
+            selectmode='browse',
+        )
+        self.timezone_listbox.grid(
+            column=1, row=8, columnspan=1, sticky='nsew', **options
+        )
+
+        # Timezone ListBox Scrollbar
+        self.scrollbar = ttk.Scrollbar(
+            self,
+            orient='vertical',
+            command=self.timezone_listbox.yview
+        )
+
+        self.timezone_listbox['yscrollcommand'] = self.scrollbar.set
+        self.scrollbar.grid(column=2, row=8, sticky='nsw', **options)
+
+        # Add Reminder Button
+        self.add_reminder_button = ttk.Button(
+            self,
+            text="Add Reminder",
+            command=lambda: controller.add_reminder(
+                self.timezone_listbox,
+                self.cal,
+                self.hours_entry,
+                self.minutes_entry,
+            )
+        )
+        self.add_reminder_button.grid(column=3, row=8, **options)
+
+        # Set Add Reminder Frame
+        self.grid(column=0, row=1, padx=5, pady=5, sticky="nsew")
+
+
+# Display Reminders Frame
+class DisplayReminders(ttk.Frame):
+    def __init__(self, container, controller):
+        super().__init__(container)
+        # field options
+        options = {'padx': 5, 'pady': 5}
+
+        # If there are Reminders
+        if controller.reminders:
+            for index, reminder in enumerate(controller.reminders):
+
+                # Display Reminder Info
+                reminder_label = ttk.Label(
+                    self,
+                    text=f"{index + 1}. {reminder.datetime_in_country} in"
+                         f" {reminder.datetime_in_country.tzinfo}"
+                )
+                reminder_label.grid(
+                    column=0, row=index + 1, columnspan=3,
+                    sticky='ew', **options
+                )
+
+                # A Cancel button for each Reminder
+                reminder_cancel_button = ttk.Button(
+                    self,
+                    text="Cancel Reminder",
+                    command=lambda: controller.cancel_reminder(
+                        controller.reminders[index].reminder_id
+                    )
+                )
+                reminder_cancel_button.grid(
+                    column=4, row=index + 1, **options
+                )
+        else:
+            ttk.Label(self, text="No Reminders Available!").grid(
+                column=2, row=2, sticky='ew', **options
+            )
+
+        # Set Display Reminders Frame
+        self.grid(column=0, row=1, padx=5, pady=5, sticky="nsew")
